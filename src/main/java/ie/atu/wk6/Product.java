0fc6fe5d11c6b88e190c298543b902e9c3f4e852
NaoiseOL <G00385498@atu.ie>
2024-10-22 10:42:05 +0100

    Added Product,ProductController and ProductService classes

--- src/main/java/ie/atu/wk6/Product.java
+++ src/main/java/ie/atu/wk6/Product.java
@@ -1,4 +1,15 @@
 package ie.atu.wk6;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+    private int id;
+    private String name;
+    private int price;
+
 }
--- src/main/java/ie/atu/wk6/ProductController.java
+++ src/main/java/ie/atu/wk6/ProductController.java
@@ -1,4 +1,31 @@
 package ie.atu.wk6;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+    private ProductService myService;
+
+    public ProductController(ProductService myService) {
+        this.myService = myService;
+    }
+
+    private List<Product> list = new ArrayList<>();
+    //creating a class which is interested in requests and responses
+
+    @PostMapping
+    public List<Product> newProduct(@RequestBody Product product){
+
+        //send it to do business logic
+        ProductService myService = new ProductService();
+        list = myService.addProduct(product);
+        return list;
+    }
 }
